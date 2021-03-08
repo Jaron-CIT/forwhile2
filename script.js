6f5c3610e1f1f145f987572de82df4181201ffbe
Jaron-CIT <78827264+Jaron-CIT@users.noreply.github.com>
2021-03-07 23:55:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var wrap = document.body.querySelector(".wrapper");
+var number = 2;
+var match=prompt("How much damage does the dragon take?")
+
+
+  match=prompt("How much damage does the user take?")
+
+Math.floor
+
+wrap.innerHTML
